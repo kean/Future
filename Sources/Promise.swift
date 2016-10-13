f4efb04907c2fbd37a20e0d13af2bd2f9a1d2ee7
kean <grebenyuk.alexander@gmail.com>
2016-10-13 11:50:57 +0300

    Update for final Swift 3 changes; Add @discardableResult where it makes sense; Use Lock instead of a DispatchQueue

--- Sources/Promise.swift
+++ Sources/Promise.swift
@@ -6,83 +6,83 @@ import Foundation
 
 public final class Promise<T> {
     private var state: State<T> = .pending(Handlers<T>())
-    private var queue = DispatchQueue(label: "com.github.kean.Promise")
-
+    private let lock = NSLock()
+    
     public init(_ closure: (_ fulfill: @escaping (T) -> Void, _ reject: @escaping (Error) -> Void) -> Void) {
         closure({ self.resolve(resolution: .fulfilled($0)) },
                 { self.resolve(resolution: .rejected($0)) })
     }
-
+    
     public init(value: T) {
         state = .resolved(.fulfilled(value))
     }
-
+    
     public init(error: Error) {
         state = .resolved(.rejected(error))
     }
-
+    
     private func resolve(resolution: Resolution<T>) {
-        queue.async {
-            if case let .pending(handlers) = self.state {
-                self.state = .resolved(resolution)
-                handlers.objects.forEach { $0(resolution) }
-            }
+        lock.lock()
+        if case let .pending(handlers) = self.state {
+            self.state = .resolved(resolution)
+            handlers.objects.forEach { $0(resolution) }
         }
+        lock.unlock()
     }
-
+    
     public func completion(on queue: DispatchQueue = .main, _ closure: @escaping (Resolution<T>) -> Void) {
         let completion: (Resolution<T>) -> Void = { resolution in
             queue.async { closure(resolution) }
         }
-        self.queue.async {
-            switch self.state {
-            case let .pending(handlers): handlers.objects.append(completion)
-            case let .resolved(resolution): completion(resolution)
-            }
+        lock.lock()
+        switch self.state {
+        case let .pending(handlers): handlers.objects.append(completion)
+        case let .resolved(resolution): completion(resolution)
         }
+        lock.unlock()
     }
 }
 
 public extension Promise {
-    public func then(on queue: DispatchQueue = .main, _ closure: @escaping (T) -> Void) -> Promise {
+    @discardableResult public func then(on queue: DispatchQueue = .main, _ closure: @escaping (T) -> Void) -> Promise {
         return then(on: queue, fulfilment: closure, rejection: nil)
     }
-
+    
     public func then<U>(on queue: DispatchQueue = .main, _ closure: @escaping (T) -> U) -> Promise<U> {
         return then(on: queue) { Promise<U>(value: closure($0)) }
     }
-
+    
     public func then<U>(on queue: DispatchQueue = .main, _ closure: @escaping (T) -> Promise<U>) -> Promise<U> {
         return Promise<U>() { fulfill, reject in
-            _ = then(
+            then(
                 on: queue,
                 fulfilment: {
-                    _ = closure($0).then(
-                        fulfilment: { _ = fulfill($0) },
-                        rejection: { _ = reject($0) })
+                    closure($0).then(
+                        fulfilment: { fulfill($0) },
+                        rejection: { reject($0) })
                 },
-                rejection: { _ = reject($0) }) // bubble up error
+                rejection: { reject($0) }) // bubble up error
         }
     }
-
-    public func `catch`(on queue: DispatchQueue = .main, _ closure: @escaping (Error) -> Void) {
-        _ = then(on: queue, fulfilment: nil, rejection: closure)
+    
+    @discardableResult public func `catch`(on queue: DispatchQueue = .main, _ closure: @escaping (Error) -> Void) {
+        then(on: queue, fulfilment: nil, rejection: closure)
     }
-
+    
     public func recover(on queue: DispatchQueue = .main, _ closure: @escaping (Error) -> Promise) -> Promise {
         return Promise() { fulfill, reject in
-            _ = then(
+            then(
                 on: queue,
-                fulfilment: { _ = fulfill($0) }, // bubble up value
+                fulfilment: { fulfill($0) }, // bubble up value
                 rejection: {
-                    _ = closure($0).then(
-                        fulfilment: { _ = fulfill($0) },
-                        rejection: { _ = reject($0) })
+                    closure($0).then(
+                        fulfilment: { fulfill($0) },
+                        rejection: { reject($0) })
             })
         }
     }
-
-    public func then(on queue: DispatchQueue = .main, fulfilment: (@escaping (T) -> Void)?, rejection: (@escaping (Error) -> Void)?) -> Promise {
+    
+    @discardableResult public func then(on queue: DispatchQueue = .main, fulfilment: ((T) -> Void)?, rejection: ((Error) -> Void)?) -> Promise {
         completion(on: queue) { resolution in
             switch resolution {
             case let .fulfilled(val): fulfilment?(val)
