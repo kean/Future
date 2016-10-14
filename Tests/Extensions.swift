8bc6e8fae46167abcbd7f2ca0e7fb206dc796ee4
kean <grebenyuk.alexander@gmail.com>
2016-10-15 00:25:55 +0300

    Start adding Promise A+ spec tests

--- Tests/Extensions.swift
+++ Tests/Extensions.swift
@@ -4,10 +4,18 @@
 
 import XCTest
 import Foundation
+import Pill
+
+
+// MARK: XCTestCase
 
 extension XCTestCase {
-    func expect(_ block: (_ fulfill: @escaping (Void) -> Void) -> Void) {
-        let expectation = makeExpectation()
+    func test(_ description: String? = nil, _ block: (Void) -> Void = {}) -> Void {
+        block()
+    }
+    
+    func expect(_ description: String = "GenericExpectation", _ block: (_ fulfill: @escaping (Void) -> Void) -> Void) {
+        let expectation = self.expectation(description: description)
         block({ expectation.fulfill() })
     }
 
@@ -30,4 +38,32 @@ func rnd() -> Int {
 
 func rnd(_ uniform: Int) -> Int {
     return Int(arc4random_uniform(UInt32(uniform)))
+}
+
+
+// MARK: Promise
+
+enum Error: Swift.Error {
+    case dummy
+    case e1
+    case e2
+}
+
+let dummy = 1
+
+extension Promise {
+    class func tuple() -> (promise: Promise, fulfill: (T) -> Void, reject: (Error) -> Void) {
+        var fulfill: ((T) -> Void)!
+        var reject: ((Error) -> Void)!
+        let promise = self.init { fulfill = $0; reject = $1 }
+        return (promise, fulfill, reject)
+    }
+    
+    class func fulfilled() -> Promise<Int> {
+        return Promise<Int>() { fulfill, _ in
+            DispatchQueue.global().async {
+                fulfill(dummy)
+            }
+        }
+    }
 }
--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -23,3 +23,78 @@ class BasicTests: XCTestCase {
         wait()
     }
 }
+
+class APlusTests: XCTestCase {
+    
+    func testThatPromiseIsCreatedInPendingState() {
+        XCTAssertEqual(Promise<Void>() { _ in }.isPending, true)
+    }
+    
+    func testThatFulfilledPromiseDoesntTransitionToOtherStates() {
+        test("2.1.2.1: When fulfilled, a promise: must not transition to any other state.")
+        
+        expect("fulfill then fulfill with different value") { finish in
+            let promise = Promise<Int>() { fulfill, _ in
+                fulfill(0)
+                fulfill(1)
+            }
+            promise.completion {
+                XCTAssertEqual($0.value, 0)
+                finish()
+            }
+        }
+        
+        expect("fulfill then reject") { finish in
+            let promise = Promise<Int>() { fulfill, reject in
+                fulfill(0)
+                reject(Error.dummy)
+            }
+            promise.completion {
+                XCTAssertEqual($0.value, 0)
+                finish()
+            }
+        }
+        
+        wait()
+    }
+    
+    func testThatRejectedPromiseDoesntTransitionToOtherStates() {
+        test("2.1.3.1: When rejected, a promise: must not transition to any other state.")
+        
+        expect("reject then reject with different error") { finish in
+            let promise = Promise<Int>() { _, reject in
+                reject(Error.e1)
+                reject(Error.e2)
+            }
+            promise.completion {
+                XCTAssertEqual($0.error as? Error, Error.e1)
+                finish()
+            }
+        }
+        
+        expect("reject then fulfill") { finish in
+            let promise = Promise<Int>() { fulfill, reject in
+                reject(Error.e1)
+                fulfill(1)
+            }
+            promise.completion {
+                XCTAssertEqual($0.error as? Error, Error.e1)
+                finish()
+            }
+        }
+        
+        wait()
+    }
+    
+    func testThen() {
+        test("2.2.2: If `onFulfilled` is a function,") {
+            expect("2.2.2.1: it must be called after `promise` is fulfilled, with `promise`’s fulfillment value as its first argument.") { finish in
+                Promise<Int>.fulfilled().then {
+                    XCTAssertEqual($0, dummy)
+                    finish()
+                }
+            }
+            wait()
+        }
+    }
+}
