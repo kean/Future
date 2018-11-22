e931602e40aa864c13d7fccd722418f9c4063ac7
kean <grebenyuk.alexander@gmail.com>
2018-11-22 23:34:29 +0100

    Add a bunch of new performance tests

--- Tests/PerformanceTests.swift
+++ Tests/PerformanceTests.swift
@@ -6,11 +6,9 @@ import XCTest
 import Foundation
 import Future
 
-class PromisePerformanceTests: XCTestCase {
+class FutureInitializationTests: XCTestCase {
 
-    // MARK: - Init
-
-    func testInit() {
+    func testInitPending() {
         measure {
             for _ in 0..<100_000 {
                 let _ = Future<Int, Void> { _ in
@@ -35,36 +33,94 @@ class PromisePerformanceTests: XCTestCase {
             }
         }
     }
+}
 
-    // MARK: - Attach Callbacks
+class FutureCallbacksTests: XCTestCase {
 
-    func testOnValue() {
+    func testAttachCallbacksToResolved() {
         let futures = (0..<50_000).map { _ in Future(value: 1) }
 
         measure {
             for future in futures {
-                future.on(success: { _ in
-                    return // do nothing
-                })
+                future.on(success: { _ in },
+                          failure: { _ in },
+                          completion: { _ in })
             }
         }
     }
 
-    func testFulfill() {
+    func testAttachCallbacksToResolvedConcurrently() {
+        let futures = Array(0..<10).map { _ in
+            Array(0..<10000).map { _ in
+                Future(value: 1)
+            }
+        }
+
+        measure {
+            DispatchQueue.concurrentPerform(iterations: 10) { iteration in
+                for future in futures[iteration] {
+                    future.on(success: { _ in },
+                              failure: { _ in },
+                              completion: { _ in })
+                }
+            }
+        }
+    }
+
+    func testAttachCallbacksToPending() {
+        let futures = (0..<50_000).map { _ in Promise<Int, Never>().future }
+
+        measure {
+            for future in futures {
+                future.on(success: { _ in },
+                          failure: { _ in },
+                          completion: { _ in })
+            }
+        }
+    }
+
+    func testAttachCallbacksToPendingTwoTimes() {
+        let futures = (0..<50_000).map { _ in Promise<Int, Never>().future }
+
+        measure {
+            for future in futures {
+                future.on(success: { _ in },
+                          failure: { _ in },
+                          completion: { _ in })
+
+                future.on(success: { _ in },
+                          failure: { _ in },
+                          completion: { _ in })
+            }
+        }
+    }
+
+    func testAttachCallbacksToPendingConcurrently() {
+        let futures = Array(0..<10).map { _ in
+            Array(0..<10000).map { _ in
+                Promise<Int, Never>().future
+            }
+        }
+
+        measure {
+            DispatchQueue.concurrentPerform(iterations: 10) { iteration in
+                for future in futures[iteration] {
+                    future.on(success: { _ in },
+                              failure: { _ in },
+                              completion: { _ in })
+                }
+            }
+        }
+    }
+}
+
+class PromiseResolveTests: XCTestCase {
+
+    func testSucceedWithOneCallback() {
         let items = (0..<100_000).map { _ in Promise<Int, Void>() }
 
-        let expectation = self.expectation()
-        var finished = 0
-
         for item in items {
-            item.future.on(success: { _ in
-                finished += 1
-                if finished == items.count {
-                    expectation.fulfill()
-                }
-
-                return // do nothing
-            })
+            item.future.on(success: { _ in })
         }
 
         measure {
@@ -72,25 +128,24 @@ class PromisePerformanceTests: XCTestCase {
                 item.succeed(value: 1)
             }
         }
-
-        wait() // wait so that next test aren't affecteds
     }
 
-    func testAttachingCallbacksToResolvedFuture() {
-        let futures = Array(0..<10000).map { _ in
-            return Future(value: 1)
+    func testSucceedWithTwoCallback() {
+        let items = (0..<100_000).map { _ in Promise<Int, Void>() }
+
+        for item in items {
+            item.future.on(success: { _ in })
+            item.future.on(success: { _ in })
         }
 
         measure {
-            for future in futures {
-                future.on(success: { _ in })
+            for item in items {
+                item.succeed(value: 1)
             }
         }
     }
 
-    // MARK: - Fulfilling From Multiple Threads
-
-    func testResolveFromMultipleThreads() {
+    func testSucceedFromMultipleThreads() {
         let promises = Array(0..<10).map { _ in
             Array(0..<10000).map { _ in
                 Promise<Int, Never>()
@@ -105,34 +160,33 @@ class PromisePerformanceTests: XCTestCase {
             }
         }
     }
+}
 
-    // MARK: - How Long the Whole Chain Takes
+class FutureChainsTests: XCTestCase {
+    // Test how fast the whole chains take
 
-    func testChain() {
+    func testChain_Map_FlatMap_MapError() {
         measure {
-            var remaining = 5000
             let expecation = self.expectation()
+            expecation.expectedFulfillmentCount = 5000
+
             for _ in 0..<5000 {
                 let future = Future<Int, Void>(value: 1)
                     .map { $0 + 1 }
                     .flatMap { Future<Int, Void>(value: $0 + 1) }
                     .mapError { $0 }
 
-                future.on(success: { _ in
-                    remaining -= 1
-                    if remaining == 0 {
-                        expecation.fulfill()
-                    }
-                })
+                future.on(success: { _ in expecation.fulfill() })
             }
             wait()
         }
     }
 
-    func testZip() {
+    func testZipArrayOf3() {
         measure {
-            var remaining = 5000
             let expecation = self.expectation()
+            expecation.expectedFulfillmentCount = 5000
+
             for _ in 0..<5000 {
                 let future = Future.zip([
                     Future<Int, Void>(value: 1),
@@ -140,19 +194,48 @@ class PromisePerformanceTests: XCTestCase {
                     Future<Int, Void>(value: 4)]
                 )
 
-                future.on(success: { _ in
-                    remaining -= 1
-                    if remaining == 0 {
-                        expecation.fulfill()
-                    }
+                future.on(success: { _ in expecation.fulfill() })
+            }
+            wait()
+        }
+    }
+
+    func testZipArrayOf1000() {
+        measure {
+            let iterations = 10
+            let futuresPerIteration = 1000
+
+            let expecation = self.expectation()
+            expecation.expectedFulfillmentCount = iterations
+
+            let promises = Array(0..<iterations).map { _ in
+                Array(0..<futuresPerIteration).map { _ in
+                    Promise<Int, Never>()
+                }
+            }
+
+            DispatchQueue.concurrentPerform(iterations: iterations) { iteration in
+                let zip = Future.zip(promises[iteration].map { $0.future })
+                zip.on(success: { _ in
+                    expecation.fulfill()
                 })
             }
+
+            DispatchQueue.global().async {
+                DispatchQueue.concurrentPerform(iterations: iterations) { iteration in
+                    for promise in promises[iteration] {
+                        promise.succeed(value: 1)
+                    }
+                }
+            }
+
             wait()
         }
     }
 }
 
-class FutureOperatorsPerformanceTests: XCTestCase {
+class FutureMapPerformanceTests: XCTestCase {
+
     func testMap() {
         let futures = Array(0..<10000).map { _ in
             Future(value: 1)
@@ -161,6 +244,66 @@ class FutureOperatorsPerformanceTests: XCTestCase {
         measure {
             for future in futures {
                 let _ = future.map { $0 + 1}
+            }
+        }
+    }
+}
+
+class FutureFlatMapPerformanceTests: XCTestCase {
+
+    func testFlatMapPending() {
+        let futures = Array(0..<10000).map { _ in
+            Promise<Int, Never>().future
+        }
+
+        measure {
+            for future in futures {
+                let _ = future.flatMap {
+                    Future(value: $0)
+                }
+            }
+        }
+    }
+
+    func testFlatMapResolvedReturnWithValue() {
+        let futures = Array(0..<10000).map { _ in
+            Future(value: 1)
+        }
+
+        measure {
+            for future in futures {
+                let _ = future.flatMap {
+                    Future(value: $0)
+                }
+            }
+        }
+    }
+
+    func testFlatMapResolvedReturnPending() {
+        let futures = Array(0..<10000).map { _ in
+            Future(value: 1)
+        }
+
+        measure {
+            for future in futures {
+                let _ = future.flatMap { _ in
+                    Promise<Int, Never>().future // pending
+                }
+            }
+        }
+    }
+}
+
+class FutureMiscPerformanceTests: XCTestCase {
+    func testObserveOn() {
+        let futures = Array(0..<10000).map { _ in
+            Future(value: 1)
+        }
+
+        let queue = DispatchQueue(label: "testObserveOn")
+        measure {
+            for future in futures {
+                let _ = future.observe(on: queue)
             }
         }
     }
